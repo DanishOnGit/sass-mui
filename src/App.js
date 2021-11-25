74cda8ece0e31dd1f66863bb00eb78910fb6808d
DanishOnGit <youcandoit1911@gmail.com>
2021-11-26 04:09:55 +0530

    initial commit:cra,sass,mui setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { CreateNote } from "./pages/CreateNote";
+
+
+function App() {
+  return (
+    <div className="App">
+     <CreateNote/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/CreateNote.jsx
@@ -0,0 +1,14 @@
+import Typography from "@mui/material/Typography";
+export const CreateNote = () => {
+  return (
+    <>
+      <Typography classes={{root:'typo'}} noWrap>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur id
+        ratione minus iure, nostrum quod voluptatum! Sint doloremque rerum
+        deleniti sequi dolore praesentium tenetur dolores, consequatur
+        molestias, totam esse laboriosam culpa illo iure, saepe temporibus modi
+        odio sapiente! Sit, numquam.
+      </Typography>
+    </>
+  );
+};
